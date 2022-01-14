c0535d9c7d55eea827627c7146f29bd8d28612cc
Hajara Iyal <hajaraiyal@gmail.com>
2022-01-14 23:58:12 +0000

    css styling and editing

--- src/db/index.js
+++ src/db/index.js
@@ -3,11 +3,9 @@ const dinoImages = [
 	"https://cdn.drawception.com/drawings/WBAMCWYk44.png",
 	"https://i.insider.com/5f08b7e5f34d053f663fbada?width=700",
 	"https://media.indiedb.com/images/games/1/56/55218/Cheeky_Dino_-_Get_redrawn.png",
-	"https://www.google.com/url?sa=i&url=https%3A%2F%2Ffineartamerica.com%2Ffeatured%2F1-cute-funny-dinocorn-dinosaur-unicorn-the-perfect-presents.html&psig=AOvVaw11QyNa_DYOUXL5V6qpxwmE&ust=1642260432924000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCODr8IDHsfUCFQAAAAAdAAAAABAa",
 	"https://ih1.redbubble.net/image.2265552562.8651/st,small,507x507-pad,600x600,f8f8f8.u1.jpg",
 	"https://cdn.vox-cdn.com/thumbor/Y-MAoTzWzY3m7aiLIkq-vtEv9b8=/0x0:1280x536/1200x800/filters:focal(538x166:742x370)/cdn.vox-cdn.com/uploads/chorus_image/image/47738175/dinotrexes.0.jpg",
 	"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/67151180-41d6-4f6e-9b9d-dbe8eda6864d/d1ppzcm-15dee335-94b3-4d7e-acad-fb7feae05a24.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY3MTUxMTgwLTQxZDYtNGY2ZS05YjlkLWRiZThlZGE2ODY0ZFwvZDFwcHpjbS0xNWRlZTMzNS05NGIzLTRkN2UtYWNhZC1mYjdmZWFlMDVhMjQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.VzM8T-DlMHQSjU-NrqSm-7_OfG_523jteMurPU7LmGE",
-	"https://www.google.com/search?q=dinosour+funny&tbm=isch&ved=2ahUKEwjzmu_8xrH1AhX3EmMBHY1uBKUQ2-cCegQIABAA&oq=dinosour+funny&gs_lcp=CgNpbWcQAzIGCAAQChAYOgcIIxDvAxAnOgUIABCABFD1CliAD2CaEGgAcAB4AIABhQGIAaEEkgEDNS4xmAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=TJbhYfPECveljLsPjd2RqAo&bih=593&biw=1280&client=firefox-b-d#imgrc=YWd7Tup5bQmcWM",
 	"https://cdn.drawception.com/images/panels/2016/5-28/gQF8DPndhE-2.png",
 ];
 
