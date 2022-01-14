2fb45928cc42e84fa2211d7d82ca304d355a7155
Lorcan Mc <lorcan.jmc@gmail.com>
2022-01-14 10:47:47 +0000

    MMVP

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-	return (
-		<div className="App">
-			<h1>Hello World</h1>
-		</div>
-	);
+  const [dinoName, setDinoName] = useState("");
+  console.log(dinoName);
+  async function getDino() {
+    const response = await fetch(
+      `https://dinoipsum.com/api/?format=json&paragraphs=1&words=100`
+    );
+    const data = await response.json();
+    const sortedName = data[0].sort();
+
+    setDinoName(sortedName[0] + " " + sortedName[1]);
+  }
+
+  useEffect(() => {
+    getDino();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>{dinoName}</p>
+    </div>
+  );
 }
 
 export default App;
