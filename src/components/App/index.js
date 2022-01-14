04d9408bc8f5441582a177492d1dfe6a0c0f48da
Lorcan Mc <lorcan.jmc@gmail.com>
2022-01-14 12:30:55 +0000

    first name, last name matching

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,48 +3,61 @@ import "./App.css";
 import Input from "../Input";
 
 function App() {
-   const [dinoName, setDinoName] = useState("");
-   const [dinoArray, setDinoArray] = useState([]);
-   const [textInput, setTextInput] = useState("");
+  const [dinoName, setDinoName] = useState("");
+  const [dinoArray, setDinoArray] = useState([]);
+  const [textInput, setTextInput] = useState("");
 
-   function handleChange(event) {
-      setTextInput(event.target.value);
-   }
+  function handleChange(event) {
+    setTextInput(event.target.value);
+  }
 
-   //    getInitials(textInput)
+  //    getInitials(textInput)
 
-   //console.log(dinoName);
-   console.log(dinoArray);
+  //console.log(dinoName);
+  console.log(dinoArray);
 
-   // Function to fetch API data and store it in dino array
-   async function getDinoArray() {
-      const response = await fetch(
-         `https://dinoipsum.com/api/?format=json&paragraphs=1&words=100`
-      );
-      const data = await response.json();
-      setDinoArray(data[0].sort());
-      //console.log(sortedName);
-   }
+  // Function to fetch API data and store it in dino array
+  async function getDinoArray() {
+    const response = await fetch(
+      `https://dinoipsum.com/api/?format=json&paragraphs=1&words=100`
+    );
+    const data = await response.json();
+    setDinoArray(data[0].sort());
+    //console.log(sortedName);
+  }
 
-   useEffect(() => {
-      getDinoArray();
-   }, []);
+  useEffect(() => {
+    getDinoArray();
+  }, []);
 
-   // Function to take two names from dinoArray into dinoName
-   function getDino() {
-      const initial = textInput.charAt(0);
-      console.log(initial);
-      setDinoName(dinoArray[0] + " " + dinoArray[1]);
-   }
-   return (
-      <div className="App">
-         <h1>Hello World</h1>
-         <p>{dinoName}</p>
-         <p>{textInput}</p>
-         {/* <p>{getInitials(textInput)}</p> */}
-         <Input getDino={getDino} onChange={handleChange} />
-      </div>
-   );
+  // Function to take two names from dinoArray into dinoName
+  function getDino() {
+
+	let splitUsername = textInput.split("");
+	let firstInitial = splitUsername[0].toUpperCase()
+	let secondInitial =splitUsername[splitUsername.indexOf(" ") + 1].toUpperCase()
+	console.log(firstInitial, secondInitial)
+
+    const matchingFirstNames = dinoArray.filter((name) => {
+      return name[0] === firstInitial;
+    });
+	const matchingSecondNames = dinoArray.filter((name) => {
+		return name[0] === secondInitial;
+	  });
+
+    console.log({ matchingFirstNames });
+    setDinoName(matchingFirstNames[0] + " " + matchingSecondNames[0]);
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>{dinoName}</p>
+      <p>{textInput}</p>
+      {/* <p>{getInitials(textInput)}</p> */}
+      <Input getDino={getDino} onChange={handleChange} />
+    </div>
+  );
 }
 
 export default App;
